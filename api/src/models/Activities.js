b9edc6176c951c394f3103eb58017564c59af8c0
ssaavedraa <santiagosaavedra98@gmail.com>
2022-02-03 10:05:37 -0500

    First commit

--- api/src/models/Activities.js
+++ api/src/models/Activities.js
@@ -1,1 +1,12 @@
-import { DataTypes } from "sequelize/dist";
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('activity', {
+        id: {
+            type:DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            allowNull: false,
+            primaryKey:true
+        }
+    })
+}
